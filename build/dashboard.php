51947f476217839aff03f6c54495334f3a854d9f
Valecoding <moiseev.kb+github@gmail.com>
2018-07-27 06:42:24 +0300

    New page dashboard

---
+++ build/dashboard.php
@@ -0,0 +1,204 @@
+<?php require "includes/header.php"; ?>
+
+
+<!--    Start of the container in the nav-left.html-->
+    <?php require "includes/nav-left.html"; ?>
+    <!--    continue of the container in the nav-left.html-->
+
+            <?php require "includes/nav-top-mobile.html"; ?>
+
+            <div class="row top-callendar">
+                <div class="with_callendar"><strong>Активность за <span class="callendar-dropdown"><a href="">сегодня<i>
+                <svg aria-hidden="true" data-prefix="fas"
+                     data-icon="chevron-down"
+                     class="svg-inline--fa fa-chevron-down fa-w-14" role="img"
+                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path
+                        fill="currentColor"
+                        d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path></svg>
+                </i></a></span></strong><span class="date">6 июля</span></div>
+                <div class="link_full_history"><a href="">Полная история активности</a></div>
+            </div>
+            <div class="table-history-activity"> <!--wrapper for list of history-->
+                <div class="row table-title d-mobil-none">
+                    <div class="col-4 name">Имя</div>
+                    <div class="col-5 process">Комментарий</div> <!--todo спросить название колонки-->
+                    <div class="col-3 time">Время</div>
+                </div>
+                <div class="main-table"> <!--main-table wrapper-->
+                    <div class="row item">
+                        <div class="col-4 name">
+                            <span class="avatar-guest"></span>
+                            <span class="name-guest">Суарез</span><br>
+                            <span class="email-guest"><a href="mailto:suarez@gmail.com">suarez@gmail.com</a></span>
+                        </div>
+                        <div class="col-5 process">
+                            Забронировал стол №17 на 21:00
+                        </div>
+                        <div class="col-3 time">
+                            2 минуты назад
+                        </div>
+                    </div>
+                    <div class="row item">
+                        <div class="col-4 name">
+                            <span class="avatar-guest"></span>
+                            <span class="name-guest">Мила</span><br>
+                            <span class="email-guest"><a href="mailto:mila@gmail.com">mila@gmail.com</a></span>
+                        </div>
+                        <div class="col-5 process">
+                            Получил заказ
+                        </div>
+                        <div class="col-3 time">
+                            27 минут назад
+                        </div>
+                    </div>
+                    <div class="row item">
+                        <div class="col-4 name">
+                            <span class="avatar-guest"></span>
+                            <span class="name-guest">Croissant</span><br>
+                            <span class="email-guest"><a href="mailto:cro@mail.ru">cro@mail.ru</a></span>
+                        </div>
+                        <div class="col-5 process">
+                            Вышел из заведения и оплатил заказ
+                        </div>
+                        <div class="col-3 time">
+                            16:30
+                        </div>
+                    </div>
+                    <div class="row item">
+                        <div class="col-4 name">
+                            <span class="avatar-guest"></span>
+                            <span class="name-guest">Евгений Формин</span><br>
+                            <span class="email-guest"><a href="mailto:eug-f@gmail.com">eug-f@gmail.com</a></span>
+                        </div>
+                        <div class="col-5 process">
+                            Приглашен пользователем Майк Ковальски
+                        </div>
+                        <div class="col-3 time">
+                            16:25
+                        </div>
+                    </div>
+                    <div class="row item">
+                        <div class="col-4 name">
+                            <span class="avatar-guest"></span>
+                            <span class="name-guest">John</span><br>
+                            <span class="email-guest"><a href="mailto:john@gmail.com">john@gmail.com</a></span>
+                        </div>
+                        <div class="col-5 process">
+                            Сел за столом №28
+                        </div>
+                        <div class="col-3 time">
+                            15:32
+                        </div>
+                    </div>
+                    <div class="row item">
+                        <div class="col-4 name">
+                            <span class="avatar-guest"></span>
+                            <span class="name-guest">Суарез Мигос</span><br>
+                            <span class="email-guest"><a href="mailto:suarez@gmail.com">suarez@gmail.com</a></span>
+                        </div>
+                        <div class="col-5 process">
+                            Забронировал стол №17 на 21:00
+                        </div>
+                        <div class="col-3 time">
+                            14:30
+                        </div>
+                    </div>
+                    <div class="row item">
+                        <div class="col-4 name">
+                            <span class="avatar-guest"></span>
+                            <span class="name-guest">Суарез</span><br>
+                            <span class="email-guest"><a href="mailto:suarez@gmail.com">suarez@gmail.com</a></span>
+                        </div>
+                        <div class="col-5 process">
+                            Забронировал стол №17 на 21:00
+                        </div>
+                        <div class="col-3 time">
+                            2 минуты назад
+                        </div>
+                    </div>
+                    <div class="row item">
+                        <div class="col-4 name">
+                            <span class="avatar-guest"></span>
+                            <span class="name-guest">Мила</span><br>
+                            <span class="email-guest"><a href="mailto:mila@gmail.com">mila@gmail.com</a></span>
+                        </div>
+                        <div class="col-5 process">
+                            Получил заказ
+                        </div>
+                        <div class="col-3 time">
+                            27 минут назад
+                        </div>
+                    </div>
+                    <div class="row item">
+                        <div class="col-4 name">
+                            <span class="avatar-guest"></span>
+                            <span class="name-guest">Croissant</span><br>
+                            <span class="email-guest"><a href="mailto:cro@mail.ru">cro@mail.ru</a></span>
+                        </div>
+                        <div class="col-5 process">
+                            Вышел из заведения и оплатил заказ
+                        </div>
+                        <div class="col-3 time">
+                            2 минуты назад
+                        </div>
+                    </div>
+                    <div class="row item">
+                        <div class="col-4 name">
+                            <span class="avatar-guest"></span>
+                            <span class="name-guest">Евгений Формин</span><br>
+                            <span class="email-guest"><a href="mailto:eug-f@gmail.com">eug-f@gmail.com</a></span>
+                        </div>
+                        <div class="col-5 process">
+                            Приглашен пользователем Майк Ковальски
+                        </div>
+                        <div class="col-3 time">
+                            2 минуты назад
+                        </div>
+                    </div>
+                    <div class="row item">
+                        <div class="col-4 name">
+                            <span class="avatar-guest"></span>
+                            <span class="name-guest">John</span><br>
+                            <span class="email-guest"><a href="mailto:john@gmail.com">john@gmail.com</a></span>
+                        </div>
+                        <div class="col-5 process">
+                            Забронировал стол №17 на 21:00
+                        </div>
+                        <div class="col-3 time">
+                            2 минуты назад
+                        </div>
+                    </div>
+                    <div class="row item">
+                        <div class="col-4 name">
+                            <span class="avatar-guest"></span>
+                            <span class="name-guest">Суарез Мигос</span><br>
+                            <span class="email-guest"><a href="mailto:suarez@gmail.com">suarez@gmail.com</a></span>
+                        </div>
+                        <div class="col-5 process">
+                            Забронировал стол №17 на 21:00
+                        </div>
+                        <div class="col-3 time">
+                            2 минуты назад
+                        </div>
+                    </div>
+                    <div class="loading-div d-none"><!--loading div-->
+                        <div class="loading-text">Загрузка...</div>
+                    </div><!--/loading-div   -->
+                </div> <!--/main-table-->
+            </div> <!--/table-history-->
+            <div class="row bottom-paginator">
+                <a href="#">Загрузить еще</a>
+                <ul class="paginator">
+                    <li><a class="active" href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#">6</a></li>
+                </ul>
+            </div>
+
+       <?php require "includes/st-content-blocks-end.html"; ?>
+
+<?php require "includes/footer-scripts.html"; ?>
+<?php require "includes/footer.html"; ?>
